0c1aac9b0414020777c1bbda9ae8d430ef93f781
shimna <shimnaa.arangil@gmail.com>
2020-09-11 17:32:20 +0530

    Simple djang REST api and frontend in react

---
+++ frontend/gui/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+
+import CustomLayout from './containers/Layout'
+import ArticleList from './containers/ArticleListView'
+
+function App() {
+  return (
+    <div className="App">
+      <CustomLayout>
+        <ArticleList />
+      </CustomLayout>
+    </div>
+  );
+}
+
+export default App;
