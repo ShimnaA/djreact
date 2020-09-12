11c1720cc15dae1a14277b14d2bedefbc6af3e04
shimna <shimnaa.arangil@gmail.com>
2020-09-12 15:34:10 +0530

    create description field in model and display in react

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-"# djreact" 
+A simple API in Django Rest Framework.The frontend uses React and queries data from the API.
--- frontend/gui/src/components/Article.js
+++ frontend/gui/src/components/Article.js
@@ -29,9 +29,7 @@ const Articles = (props) => {
           <List.Item
             key={item.title}
             actions={[
-              <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-              <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
-              <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+              
             ]}
             extra={
               <img
