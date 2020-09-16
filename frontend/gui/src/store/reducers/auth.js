c863b07006fe3547a9af888fb5524734b8c9241a
shimna <shimnaa.arangil@gmail.com>
2020-09-16 17:51:29 +0530

    authenticate frontend

---
+++ frontend/gui/src/store/reducers/auth.js
@@ -0,0 +1,30 @@
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
+
+const intialState = {
+    token: null,
+    errors: null,
+    loading: false
+}
+
+const authStart = (state, action) => {
+    return updateObject(state, {
+        error: null,
+        loading: true
+    })
+}
+
+const authSuccess = (state, action) => {
+    return updateObject(state, {
+        token: action.token,
+        error: null,
+        loading: false
+    })
+}
+
+const authFail = (state, action) => {
+    return updateObject(state, {
+        error: action.error,
+        loading: false
+    })
+}
